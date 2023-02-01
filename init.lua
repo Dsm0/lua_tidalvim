6f3a63a46719309776ccb639adef0f6845a1f779
Dsm0 <willrinkoff@gmail.com>
2023-01-31 22:23:46 -0500

    added basic snippet features

--- init.lua
+++ init.lua
@@ -3,6 +3,15 @@
 
 -- NOTE: needs to be the root directory of lua_tidalvim
 
+
+
+-- https://stackoverflow.com/a/69142336
+local function t(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+
+
 -- vim.g.tidalvim_root = os.getenv("TIDALVIM_ROOT") .. 
 vim.g.tidalvim_root = 
   os.getenv("HOME") .. '/.local/share/lua_tidalvim'
@@ -18,11 +27,36 @@ vimTidal.defaults.set_default_settings()
 vimTidal.defaults.load_default_plugins()
 
 
-local luasnip = require('luasnip.loaders.from_snipmate')
-                  .load({paths = '/snippets'})
+-- anything past this line I consider my own configuration
 
--- require('../plugins/l')
--- require("luasnip.loaders.from_snipmate")
+local ls = require('luasnip')
+require('luasnip.loaders.from_snipmate')
+                  .load({paths = 'snippets'})
 
--- vim.api.nvim_set_keymap('x',"<c-d>", ':NERDTreeToggle<CR>',{})
--- vim.api.nvim_exec("imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? 'luasnip-expand-or-jump' : '<Tab>' ",{})
+local expandSnippet = function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end
+
+
+-- NOTE: this lets me create abbreviations for snippets
+-- if I have the snippet: orbit -> (# orbit ($1/8))
+-- I can abbreviate it with just o, then try to expand it twice
+-- o -> orbit -> (# orbit ($1/8))
+-- ...
+-- This decision might come back to haunt me but there are not implementing
+-- snippet aliases and I'm not going to lol https://github.com/garbas/vim-snipmate/issues/124
+local doubleExpand = function()
+  if ls.expand_or_jumpable() 
+    then
+      ls.expand_or_jump()
+      if ls.expandable() then 
+        ls.expand()
+      end
+    else
+      vim.cmd(t("<Tab>"))
+  end
+end
+
+vim.keymap.set({"i"}, "<Tab>", doubleExpand , { silent = true })
